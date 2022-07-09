a9ce763a1f33c9d5dbda637f3b1b241f02ec30a1
Eyal <rabeyal123@gmail.com>
2022-07-10 01:06:35 +0300

    fetch recipes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
      <h1>Hello</h1>
+     <Pages />
 
     </div>
   );
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,11 @@
+import Home from "../pages/Home";
+
+function Pages() {
+  return (
+    <div>
+        <Home />
+    </div>
+  )
+}
+
+export default Pages
