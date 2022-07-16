f720c3629b106a4ab396ee897234b24c330945dc
Eyal <rabeyal123@gmail.com>
2022-07-16 16:54:56 +0300

    get recipes in popular page"

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
-    useEffect(()=> {
-        getPopular();
-    },[]);
 
-    const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-        const data = await api.json();
-        console.log(data);
-    };
+  const [popular, setPopular] = useState([]);
+  useEffect(()=> {
+      getPopular();
+  },[]);
+
+  const getPopular = async () => {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+      const data = await api.json();
+      setPopular(data.recipes);
+  };
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map((recipe) => {
+        return(
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
