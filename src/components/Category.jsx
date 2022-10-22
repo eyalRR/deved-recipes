4b86f580a587727f0f399e775dc2e8d65c150df2
Eyal <rabeyal123@gmail.com>
2022-10-22 22:07:38 +0300

    category bar decoration

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,27 +1,29 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import { TbSoup } from 'react-icons/tb';
+
 import styled from 'styled-components';
 import {NavLink} from 'react-router-dom';
 
 function Category() {
   return (
     <List>
-        <NavLink to={'/cuisine/Italian'}>
+        <SLink to={'/cuisine/Italian'}>
             <FaPizzaSlice />
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
+        </SLink>
+        <SLink to={'/cuisine/American'}>
             <FaHamburger />
             <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Thai'}>
+        </SLink>
+        <SLink to={'/cuisine/Thai'}>
             <GiNoodles />
             <h4>Thaifood</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Jewish'}>
-            <GiChopsticks />
-            <h4>Japanese</h4>
-        </NavLink>
+        </SLink>
+        <SLink to={'/cuisine/Jewish'}>
+            <TbSoup />
+            <h4>Jewish</h4>
+        </SLink>
     </ List>
   )
 }
@@ -30,6 +32,33 @@ const List = styled.div`
     display: flex;
     justify-content: center;
     margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4 {
+        color: white;
+        font-size: 0.8rem;
+    }
+    svg {
+        color: white;
+        font-size: 1.5rem;
+    }
+    &.active {
+        background: linear-gradient(to right, #f27121, #e94057);
+    }
 `;
 
 export default Category
