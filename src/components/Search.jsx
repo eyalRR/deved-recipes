126b0deeee0ce94378d8b272de8debb0f2cfb737
Eyal <rabeyal123@gmail.com>
2022-10-23 21:30:56 +0300

    working search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import {useNavigate} from 'react-router-dom';
 
 
 function Search() {
     const [input, setInput] = useState("");
+    const navigate = useNavigate();
 
     const submitHandler = (e) => {
         e.preventDefault();
+        navigate('/searched/' + input);
     }
 
   return (
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,34 +1,35 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { Link, useParams } from 'react-router-dom';
 
-function Cuisine() {
-    const [cuisine, setCuisine] = useState([]);
+
+function Searched() {
+
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
     let params = useParams();
 
-    const getCuisine = async (name) => {
+    const getSearched = async (name) => {
         const data = await fetch(
-            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
         );
         const recipes = await data.json();
-        setCuisine(recipes.results);
+        setSearchedRecipes(recipes.results);
     };
 
     useEffect(() => {
-        getCuisine(params.type);
-    }, [params.type]);
+        getSearched(params.search);
+    }, [params.search]);
 
   return (
     <Grid>
-        {cuisine.map((item) => {
-            return(
+        {searchedRecipes.map((item) => {
+            return (
                 <Card key={item.id}>
                     <img src={item.image} alt="" />
                     <h4>{item.title}</h4>
                 </Card>
             )
-        })} 
+        })}
     </Grid>
   )
 }
@@ -38,7 +39,6 @@ const Grid = styled.div`
     grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
     grid-gap: 3rem;
 `;
-
 const Card = styled.div`
     img {
         width: 100%;
@@ -53,4 +53,4 @@ const Card = styled.div`
     }
 `;
 
-export default Cuisine;
+export default Searched
