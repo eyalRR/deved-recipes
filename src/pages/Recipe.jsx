3c855031838f06bd6fdef54687151730a3a8bd56
Eyal <rabeyal123@gmail.com>
2022-11-01 12:19:58 +0200

    recipe dexcription styling before create elements

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -4,9 +4,60 @@ import { useParams } from "react-router-dom";
 
 
 function Recipe() {
+
+  let params = useParams();
+  const [details, setDetails] = useState({});
+  
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+
+useEffect(() => {
+  fetchDetails();
+}, [params.name]);
+
   return (
-    <div>Recipe</div>
+    <div>{details.title}</div>
   )
 }
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active{
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2{
+    margin-bottom: 2rem;
+  }
+  li{
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul{
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+const Info = styled.div`
+  margin-left: 10rem;
+`;
+
+
+
 
 export default Recipe
