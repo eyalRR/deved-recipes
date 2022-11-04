3ce30442f793382c0143ae28af680110c94d424d
Eyal <rabeyal123@gmail.com>
2022-11-04 17:49:13 +0200

    add logo and name with link to home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import {BrowserRouter} from 'react-router-dom';
+import {Link, BrowserRouter} from 'react-router-dom';
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}> deliciousss </Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -14,5 +20,22 @@ function App() {
     </div>
   );
 }
+
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-family: 'Lobster Two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`
 
 export default App;
