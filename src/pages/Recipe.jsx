3593efdf8b67db427a74fa1cac94861d915c402e
Eyal <rabeyal123@gmail.com>
2022-11-04 17:41:44 +0200

    link every recipe img to recipe page + activate instructions and ingredients

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -41,16 +41,20 @@ useEffect(() => {
           Ingredients
         </Button>
 
-        <div>
+        {activeTab === 'instructions' && (
+          <div>
           <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
           <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
         </div>
-
-        <ul>
+        )}
+        {activeTab === 'ingredients' &&(
+          <ul>
           {details.extendedIngredients.map((ingredient) => (
             <li key={ingredient.id}>{ingredient.original}</li>
           ))}
         </ul>
+        )}
+        
       </Info>
     </DetailWrapper>
   )
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 
@@ -23,10 +23,12 @@ function Searched() {
   return (
     <Grid>
         {searchedRecipes.map((item) => {
-            return (
+            return (               
                 <Card key={item.id}>
-                    <img src={item.image} alt="" />
-                    <h4>{item.title}</h4>
+                    <Link to={'/recipe/' + item.id}>
+                        <img src={item.image} alt="" />
+                        <h4>{item.title}</h4>
+                    </Link>
                 </Card>
             )
         })}
